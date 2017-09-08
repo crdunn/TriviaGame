569b76da864be87502a0619aa549eb71e1f4ec25
Charles Dunn <charlesdunn@Charless-MBP.home>
2017-09-07 21:24:57 -0400

    pictures

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,6 +29,7 @@ var question = 0
 
 function questionStart () {
 	$(".image").html(" ");
+	$(".answer").html("Answer:")
 	question++
 	$(".questionCounter").html(question);
 	displayTime = setInterval(decrement, 1000);
@@ -62,14 +63,14 @@ function firstQuestion() {
 };
 
 function firstAnswer() {
-	$(".question").html("Correct Answer: Scarlet Witch");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Answer: The founding Avengers were Captain America, Iron Man, The Hulk, Thor, Black Widow, and Hawkeye.");
+	$(".image").html("<img src='../images/AvengersGroupShot.jpg' alt='image'>");
 	winCheck();
 
 };
 
 function secondQuestion() {
-	$(".question").html("In her first appearence in comics, what motivated Diana of Themiscara to fight for the Allies in WWII?");
+	$(".question").html("In her first appearence in comics, what motivated Wonder Woman to fight for the Allies in WWII?");
 	$(".a1").html('<label class="radio-inline"><input type="radio" name="q1"> Defending her homeland</label>');
 	$(".a2").html('<label class="radio-inline"><input type="radio" name="q1"> A command form Ares, the God of War</label>');
 	$(".a3").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Defending the Greek principals of democracy across the world</label>');
@@ -78,8 +79,8 @@ function secondQuestion() {
 };
 
 function secondAnswer() {
-	$(".question").html("Correct Answer: Defending the Greek principals of democracy.");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Answer: Defending the Greek principals of democracy.  Her mother, Queen Hippolyta, saw the United States as the last, best, hope for the light of Democracy in a world that was swiftly falling into totalarianism.");
+	$(".image").html("<img src='../images/wwcostume.jpg' alt='image'>");
 	winCheck ();
 };
 
@@ -94,15 +95,15 @@ function thirdQuestion() {
 };
 
 function thirdAnswer() {
-	$(".question").html("Correct Answer: Batman");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Correct Answer: Batman.  Superman trusted Batman's judgment and morality more than anyone else on the planet.");
+	$(".image").html("<img src='../images/kryptonite.jpg' alt='image'>");
 	winCheck();
 
 
 };
 
 function fourthQuestion() {
-	$(".question").html("In the Image Comic series <b>Saga</b>, what animal companion started as the partner of the Bounty Hunter The Will?");
+	$(".question").html("In the Image Comic series <b>Saga</b>, what animal companion started as the partner of the Bounty Hunter, 'The Will'?");
 	$(".a1").html('<label class="radio-inline"><input type="radio" name="q1"> Devil Dog</label>');
 	$(".a2").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Lying Cat</label>');
 	$(".a3").html('<label class="radio-inline"><input type="radio" name="q1"> Storm Crow</label>');
@@ -112,14 +113,14 @@ function fourthQuestion() {
 };
 
 function fourthAnswer() {
-	$(".question").html("Correct Answer: Lying Cat");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Correct Answer: Lying Cat.  Lying Cats always know hen someone is lying, and will say if they are.  A useful tool for a bounty hunter.");
+	$(".image").html("<img src='../images/lyingCat.jpg' alt='image'>");
 	winCheck();
 
 };
 
 function fifthQuestion() {
-	$(".question").html("Which of the following ridiculous belongs to an actual superhero in mainline DC/Marvel continunity?");
+	$(".question").html("Which of the following ridiculous names belongs to an actual superhero in mainline DC/Marvel continunity?");
 	$(".a1").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Arm-Fall-Off Boy</label>');
 	$(".a2").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Matter-Eater Lad</label>');
 	$(".a3").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Mister Sinister</label>');
@@ -129,13 +130,20 @@ function fifthQuestion() {
 };
 
 function fifthAnswer() {
-	$(".question").html("Correct Answer: Trick Question.  They're all real.");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Correct Answer: Trick Question.  They're all real.");
+	$(".image1").html("<img src='../images/AFOB.jpg' alt='image'>");
+	$(".image2").html("<img src='../images/mel.jpg' alt='image'>");
+	$(".image3").html("<img src='../images/misterSinister.png' alt='image'>");
+	$(".image4").html("<img src='../images/ladystiltman.jpg' alt='image'>");
 	winCheck();
 
 };
 
 function sixthQuestion() {
+	$(".image1").html("  ");
+	$(".image2").html("  ");
+	$(".image3").html("  ");
+	$(".image4").html("  ");
 	$(".question").html("The protagonist of Neil Gaimen's fantasy comic series 'Sandman', is Dream the Endless.  Which of his six siblings does he get along the best with?");
 	$(".a1").html('<label class="radio-inline correct"><input type="radio" name="q1" class="correctAns"> Death the Endless</label>');
 	$(".a2").html('<label class="radio-inline"><input type="radio" name="q1"> Destiny the Endless</label>');
@@ -146,8 +154,8 @@ function sixthQuestion() {
 };
 
 function sixthAnswer() {
-	$(".question").html("Correct Answer: Death the Endless");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("Correct Answer: Death the Endless.  Dispite their clashing personalities, Death's upbeat outlook on this prescious gift called 'life' often drags Dream out of his dour moods.");
+	$(".image").html("<img src='../images/deathDream.jpg' alt='image'>");
 	winCheck();
 
 };
@@ -163,8 +171,8 @@ function seventhQuestion() {
 };
 
 function seventhAnswer() {
-	$(".question").html("There is no 'correct' answer.  I'm just glad you have a favorite");
-	$(".image").html("<img src='http://via.placeholder.com/350x150' alt='image'>");
+	$(".answer").html("There is no 'correct' answer!  They're all great!    I'm just glad you have a favorite.<br>Or maybe you don't.  That's OK too!");
+	$(".image").html("<img src='../images/falloutUp.jpg' alt='image'>");
 	winCheck();
 
 };
